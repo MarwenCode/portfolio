da091a50b286460c60b34b1db889bacfbf250fb2
MarwenCode <marwen_jdidi@yahoo.com>
2023-02-02 00:17:08 +0100

    portfolio download

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -25,7 +25,8 @@ const Home = () => {
         <div className="left">
           <h2>Marwen Jdidi</h2>
           <span>React JS developer</span>
-          <span className="cv" onClick={handleDownload}>
+          {/* <span className="cv" onClick={handleDownload}> */}
+          <span className="cv" >
             Download CV
             <abbr title="clic here to download CV">
               <GrDownload style={{ fontSize: "20px" }} className="icon" />
